3645d797cd754eb993aee43fd142a9c97cd7f59e
sms <shatild12@gmail.com>
2022-06-09 01:22:06 +0600

    Create body,appbar,bottomNavigationBar

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -5,6 +5,39 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.home,
+              ),
+              label: 'Home'),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.favorite,
+              ),
+              label: 'Favorite'),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.notifications,
+              ),
+              label: 'Notifications'),
+        ],
+      ),
+      body: Column(
+        children: const [
+          /// Find the best coffee for you
+          ///
+          /// Search bar
+          ///
+          /// Horizontal listview of coffee tiles
+        ],
+      ),
+    );
   }
 }
