b2684f1aa88ffaed0601f0ee9976eabd38aa0bee
Sarwar Murshed Shatil <38027343+shatilsarower@users.noreply.github.com>
2022-08-23 02:31:31 +0600

    Decorate the middle section.

--- lib/pages/coffee_tile.dart
+++ lib/pages/coffee_tile.dart
@@ -8,12 +8,28 @@ class CoffeeTile extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.only(left: 25.0, bottom: 25),
       child: Container(
+        padding: const EdgeInsets.all(12),
         width: 200,
         decoration: BoxDecoration(
           color: Colors.black54,
           borderRadius: BorderRadius.circular(14),
         ),
-        child: Image.asset('asstes/image/lattey.jpg'),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            //Coffee Image
+            ClipRRect(
+              borderRadius: BorderRadius.circular(12.0),
+              child: Image.asset('asstes/image/lattey.jpg'),
+            ),
+            //Coffee Name
+            const Text(
+              'Latte',
+              style: TextStyle(fontSize: 20),
+            ),
+            const Text('With full cream milk'),
+          ],
+        ),
       ),
     );
   }
